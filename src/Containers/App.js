80aabfe797f7c307540b91eb49f4eaba43859341
Bruno <bruno@MacBook-Air.local>
2019-05-29 19:55:33 -0300

    first commit

---
+++ src/Containers/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import CardList from '../Components/CardList'
+import SearchBox from '../Components/SearchBox'
+import './App.css'
+import Scroll from '../Components/Scroll'
+import ErrorBoundary from '../Components/ErrorBoundary'
+
+
+class App extends Component {
+	constructor(){
+		super();
+		this.state = {
+			robots: [],
+			searchField: ''
+		}
+	}
+
+	componentDidMount(){
+		console.log(this.state.robots);
+		fetch('https://jsonplaceholder.typicode.com/users').then(response =>{
+			return response.json();
+		})
+		.then(users => {
+			this.setState({robots: users});
+		});
+	}
+
+	onSearchChange = (evt) => {
+		this.setState({ searchField: evt.target.value })
+	}
+
+	render(){
+		const { robots, searchField } = this.state;
+		
+		const filteredRobots = robots.filter(robot => {
+			return robot.name.toLowerCase().includes(searchField.toLowerCase());
+		});
+
+		console.log(this.state.robots);
+		console.log({robots});
+		console.log(filteredRobots);
+
+		return !robots.length ?
+		<h1>Loading...</h1> :
+		(
+			<div className='tc'>
+				<h1 className='f1'>RoboFriends</h1>
+				<SearchBox searchChange={this.onSearchChange}/>
+				<Scroll>
+					<ErrorBoundary>
+						<CardList robots={filteredRobots} />
+					</ErrorBoundary>
+				</Scroll>
+		
+			</div>
+		);
+	
+	}
+
+}
+
+
+export default App
