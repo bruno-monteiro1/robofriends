fd33d0877d6fba07f4861764dfc20d7b095a6bf6
Bruno <brunobapmon@gamil.com>
2019-10-09 19:06:36 -0300

    Updates

---
+++ precache-manifest.a8955adaae9e7d7fccd1ceb5cd5ee59b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5641ffa65b95b96a200d9274112440d2",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "22b92ca6256f48fc6a44",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "32830c8e3b4844ec0339",
+    "url": "/robofriends/static/css/main.4f479737.chunk.css"
+  },
+  {
+    "revision": "22b92ca6256f48fc6a44",
+    "url": "/robofriends/static/js/2.31bf04af.chunk.js"
+  },
+  {
+    "revision": "32830c8e3b4844ec0339",
+    "url": "/robofriends/static/js/main.2a206191.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
